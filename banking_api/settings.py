520dad1b3e0f9b61d775ce45626f43cdab9bf30f
Vladislav Klass <vladislavklass@web.de>
2022-09-23 17:24:47 +0200

    initial swagger setup

--- banking_api/settings.py
+++ banking_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'drf_yasg',
     'rest_framework',
     'banking_api',
     'django.contrib.admin',
--- banking_api/urls.py
+++ banking_api/urls.py
@@ -15,9 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from banking_api import views
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Entrix Banking API",
+      default_version='v1',
+      description="This is the entrix banking API",
+    #   terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="vladislavklass@web.de"),
+      license=openapi.License(name="Test License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,7 +41,10 @@ urlpatterns = [
     path('accounts/<int:id>/balance', views.account_balance),
     path('accounts/<int:id>/transfers', views.account_transfers),
     path('customers/', views.customer_list),
-    path('transfers/', views.transfer_list) 
+    path('transfers/', views.transfer_list),
+    path('swagger(<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
